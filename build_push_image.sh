2c5be28a3e68dfa8cfb417ad10bfd5f9e7a0a645
dhino12 <dhinorahmad0@gmail.com>
2025-01-06 13:30:28 +0700

    try to fix build_push_image & docker-compose.yaml

--- build_push_image.sh
+++ build_push_image.sh
@@ -5,6 +5,7 @@ docker build -t item-app:v1 .
 echo "Listing Docker images..."
 docker images
 
+echo "==== Make sure there is a repository called 'item-app' in your DockerHub ===="
 echo "Please enter your Docker Hub Username..."
 read USERNAME_DOCKER_HUB
 
@@ -14,19 +15,12 @@ read -s PASSWORD_DOCKER_HUB
 echo "Tagging item-app:v1 image for Docker Hub..."
 docker tag item-app:v1 $USERNAME_DOCKER_HUB/item-app:v1
 
-# echo "Pulling mongo:3 image..."
 docker pull mongo:3
 
-# echo "Creating item-db network..."
 docker network create item-db
-
-# echo "Creating app-db volume..."
 docker volume create app-db
 
-# echo "Running item-app container..."
 docker run -dp 8080:8080 --name item-app -w /app -v "$(pwd):/app" --network item-db -e NODE_ENV=production -e DB_HOST=item-db node:14-alpine sh -c "npm install --unsafe-perm && npm run build && npm start"
-
-# echo "Running MongoDB container..."
 docker run -d --name item-db --network item-db --network-alias mongo -v app-db:/data/db -e NODE_ENV=production -e DB_HOST=item-db mongo:3
 
 docker commit item-app items-app
